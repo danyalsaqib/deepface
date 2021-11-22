1b3d01bcf99aa3dba77ed5af1fbc51d6ebceb86b
Danyal Saqib <dsaqib.bee17seecs@seecs.edu.pk>
2021-11-22 10:08:33 +0500

    Update functions.py

--- deepface/commons/functions.py
+++ deepface/commons/functions.py
@@ -166,9 +166,12 @@ def preprocess_face(img, target_size=(224, 224), grayscale = False, enforce_dete
 
 	#img might be path, base64 or numpy array. Convert it to numpy whatever it is.
 	img = load_image(img)
+
 	if img is None:
 		print("Can't Load Image, skipping this image\n")
-        	return -1
+		lol = -1
+        	return lol
+	
 	base_img = img.copy()
 
 	img, region = detect_face(img = img, detector_backend = detector_backend, grayscale = grayscale, enforce_detection = enforce_detection, align = align)
